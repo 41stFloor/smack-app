ea1debcce38efc2a53cd3bc604d526325ab8feee
Jonah Travis <master@Jonahs-iMac.local>
2019-09-29 20:12:29 -0400

    Added GradientView, buttons, labels, and TableView.

---
+++ smack-app/smack-app/View/GradientView.swift
@@ -0,0 +1,37 @@
+//
+//  GradientView.swift
+//  smack-app
+//
+//  Created by Jonah Travis on 9/29/19.
+//  Copyright © 2019 Jonah Travis. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class GradientView: UIView {
+
+    @IBInspectable var topColor: UIColor = #colorLiteral(red: 0.3631127477, green: 0.4045833051, blue: 0.8775706887, alpha: 1) {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    @IBInspectable var bottomColor: UIColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1) {
+          didSet {
+              self.setNeedsLayout()
+          }
+      }
+    
+
+    override func layoutSubviews() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        gradientLayer.frame = self.bounds
+        self.layer.insertSublayer(gradientLayer, at: 0)
+        
+    }
+    
+}
