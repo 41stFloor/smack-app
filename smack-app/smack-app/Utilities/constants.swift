7deea8fa0b55fa19ab7b94819bf57b3c7c5da331
Jonah Travis <master@Jonahs-iMac.local>
2019-09-29 20:57:09 -0400

    Adding Login View

---
+++ smack-app/smack-app/Utilities/constants.swift
@@ -0,0 +1,14 @@
+//
+//  constants.swift
+//  smack-app
+//
+//  Created by Jonah Travis on 9/29/19.
+//  Copyright © 2019 Jonah Travis. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+let TO_LOGIN = "toLogin"
+
+
---
+++ smack-app/smack-app/View/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginVC.swift
+//  smack-app
+//
+//  Created by Jonah Travis on 9/29/19.
+//  Copyright © 2019 Jonah Travis. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
